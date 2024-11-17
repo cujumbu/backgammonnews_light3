8f41d0deea6fc3881b3933bab8943f19964d66d6
cujumbu <nethandelskompagniet@gmail.com>
2024-11-17 09:54:23 +0100

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,7 @@
 /// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly PORT: string;
+  readonly NODE_ENV: 'development' | 'production';
+}
