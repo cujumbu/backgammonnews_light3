e3a3d5c159aba21478d5590cbbf7702d8adf208f
cujumbu <nethandelskompagniet@gmail.com>
2024-11-17 13:26:05 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import react from "@astrojs/react";
 import node from "@astrojs/node";
+import react from "@astrojs/react";
 
 export default defineConfig({
   output: 'server',
@@ -13,6 +13,7 @@ export default defineConfig({
     host: '0.0.0.0',
     port: Number(process.env.PORT) || 10000
   },
+  site: 'https://backgammon-news.onrender.com',
   vite: {
     ssr: {
       noExternal: ['cheerio', 'parse5', 'parse5-htmlparser2-tree-adapter']
