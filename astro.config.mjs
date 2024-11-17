c258885fcb2be8eb77d5e5367e1c1dca49dc5973
cujumbu <nethandelskompagniet@gmail.com>
2024-11-17 12:28:03 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import node from "@astrojs/node";
 import react from "@astrojs/react";
+import node from "@astrojs/node";
 
 export default defineConfig({
   output: 'server',
