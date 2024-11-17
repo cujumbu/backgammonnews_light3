1c39e7d0541230403dd236dc4eb35549b80718ac
cujumbu <nethandelskompagniet@gmail.com>
2024-11-17 09:48:27 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,17 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import node from "@astrojs/node";
 
+// Get port from environment variable or default to 10000
+const PORT = process.env.PORT || 10000;
+
 export default defineConfig({
   output: 'server',
   adapter: node({
     mode: "standalone"
   }),
+  server: {
+    port: PORT,
+    host: '0.0.0.0'
+  },
   integrations: [tailwind()]
 });
