9db79c59a2949bcc70221bc08916b55632e3906e
cujumbu <nethandelskompagniet@gmail.com>
2024-11-17 09:57:26 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,8 +8,8 @@ export default defineConfig({
     mode: "standalone"
   }),
   server: {
-    host: process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost',
-    port: process.env.PORT || 10000
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 10000
   },
   integrations: [tailwind()]
 });
